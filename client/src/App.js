c851cb37a4934c313a8fe5ac595672256cbf3529
charlottechen1993 <charlottechen21@gmail.com>
2021-01-18 22:05:16 -0500

    import material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
+import "./App.scss";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
 } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import Home from './containers/Home';
 import About from './containers/About';
 import Experiences from './containers/Experiences';
@@ -13,30 +17,42 @@ function App() {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/experiences">Experiences</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/experiences">
-                        <Experiences />
-                    </Route>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6">
+                        Charlotte Chen
+                    </Typography>
+
+                    <Link to="/">
+                        <Button color="inherit">
+                            Home
+                        </Button>
+                    </Link>
+                    <Link to="/about">
+                        <Button color="inherit">
+                            About
+                        </Button>
+                    </Link>
+                    <Link to="/experiences">
+                        <Button color="inherit">
+                        Experiences
+                        </Button>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+
+            <Switch>
+                <Route path="/experiences">
+                    <Experiences />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
             </div>
         </Router>
     );
