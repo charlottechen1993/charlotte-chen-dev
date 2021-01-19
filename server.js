5a3e350f2c3da67e3013570a2ecad02ddc6c91c9
charlottechen1993 <charlottechen21@gmail.com>
2021-01-18 23:01:57 -0500

    profile for heroku

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api/experiences', (req, res) => {
     res.json(experiences);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
