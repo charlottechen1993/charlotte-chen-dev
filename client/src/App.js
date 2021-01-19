cc6678df58d6d8da96358b913b5e93c9c4c92c98
charlottechen1993 <charlottechen21@gmail.com>
2021-01-18 22:50:39 -0500

    ready for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -5,56 +5,87 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import {
+    AppBar,
+    Typography,
+    Toolbar,
+    Button,
+    ThemeProvider,
+    Container
+} from '@material-ui/core'
 import Home from './containers/Home';
 import About from './containers/About';
 import Experiences from './containers/Experiences';
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blueGrey } from '@material-ui/core/colors';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: blueGrey[50],
+      main: blueGrey[500],
+      dark: blueGrey[900],
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
+
 function App() {
     return (
-        <Router>
-            <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography
-                        variant="h6">
-                        Charlotte Chen
-                    </Typography>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <AppBar
+                    position="static"
+                    color="transparent">
+                    <Toolbar>
+                        <Typography
+                            variant="h6">
+                            Charlotte Chen
+                        </Typography>
 
-                    <Link to="/">
-                        <Button color="inherit">
-                            Home
-                        </Button>
-                    </Link>
-                    <Link to="/about">
-                        <Button color="inherit">
-                            About
-                        </Button>
-                    </Link>
-                    <Link to="/experiences">
-                        <Button color="inherit">
-                        Experiences
-                        </Button>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-
-            <Switch>
-                <Route path="/experiences">
-                    <Experiences />
-                </Route>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-            </div>
-        </Router>
+                        <div>
+                            <Link to="/">
+                                <Button color="inherit">
+                                    Home
+                                </Button>
+                            </Link>
+                            <Link to="/about">
+                                <Button color="inherit">
+                                    About
+                                </Button>
+                            </Link>
+                            <Link to="/experiences">
+                                <Button color="inherit">
+                                Experiences
+                                </Button>
+                            </Link>
+                        </div>
+                        
+                    </Toolbar>
+                </AppBar>
+                <Container>
+                    <Switch>
+                        <Route path="/experiences">
+                            <Experiences />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Container>
+            </Router>
+        </ThemeProvider>
     );
 }
 
