294b5cb3618e2fb6a9ab733c2ac803fb1ea8b7bf
charlottechen1993 <charlottechen21@gmail.com>
2021-01-20 23:39:21 -0500

    social icons

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {
-    Grid
-} from '@material-ui/core'
+    Grid,
+    IconButton
+} from '@material-ui/core';
+import {
+    GitHub,
+    Facebook,
+    Instagram,
+    LinkedIn
+} from '@material-ui/icons';
 
 const Home = () => {
     return (
@@ -9,9 +16,17 @@ const Home = () => {
             container
             direction="row"
             justify="center"
-            alignItems="center"
-            >
+            alignItems="center">
             <h1>Hi, I'm Charlotte</h1>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center">
+                <IconButton href="https://github.com/charlottechen1993" target="_blank"><GitHub fontSize="small" /></IconButton>
+                <IconButton href="https://www.linkedin.com/in/charlotte-chen-b6200781/" target="_blank"><LinkedIn fontSize="small" /></IconButton>
+                <IconButton href="https://www.instagram.com/charlottechen1993/" target="_blank"><Instagram fontSize="small" /></IconButton>
+            </Grid>
         </Grid>
         
     )
